cec1620040c66772f7a6650af09b8fc3b8b0d035
Kai Siemek <kai.siemek@icloud.com>
2026-04-21 23:46:44 +0900

    feat: add basic event loop architecture

--- src/server.rs
+++ src/server.rs
@@ -3,21 +3,56 @@ use std::net::SocketAddr;
 use anyhow::Result;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc;
 
 const DEFAULT_SERVER_SOCKET: &str = "127.0.0.1:55123";
+
+#[derive(Debug)]
+enum Event {
+    NewConnection { stream: TcpStream, addr: SocketAddr },
+}
 
 pub async fn start() -> Result<()> {
     let listener = TcpListener::bind(DEFAULT_SERVER_SOCKET).await?;
     log::info!("started listening on port {}", listener.local_addr()?);
+    let (event_tx, event_rx) = mpsc::unbounded_channel::<Event>();
+    let server_event_tx = event_tx.clone();
+    tokio::spawn(async move {
+        if let Err(err) = run_server_loop(listener, server_event_tx.clone()).await {
+            log::error!("an error occurred in the sever thread: {}", err);
+        }
+    });
+    run_event_loop(event_tx, event_rx).await?;
+    Ok(())
+}
 
+async fn run_event_loop(
+    sender: mpsc::UnboundedSender<Event>,
+    mut receiver: mpsc::UnboundedReceiver<Event>,
+) -> Result<()> {
+    log::info!("running event loop");
+    while let Some(event) = receiver.recv().await {
+        match event {
+            Event::NewConnection { stream, addr } => {
+                tokio::spawn(async move {
+                    if let Err(err) = handle_client(stream, addr).await {
+                        log::error!("an error occured while handling client {}: {}", addr, err);
+                    }
+                });
+            }
+        }
+    }
+    log::info!("event loop has finished");
+    Ok(())
+}
+
+async fn run_server_loop(
+    listener: TcpListener,
+    sender: mpsc::UnboundedSender<Event>,
+) -> Result<()> {
     loop {
         let (stream, addr) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            if let Err(err) = handle_client(stream, addr).await {
-                log::error!("an error occured while handling client {}: {}", addr, err);
-            }
-        });
+        sender.send(Event::NewConnection { stream, addr })?;
     }
 }
 async fn handle_client(mut stream: TcpStream, addr: SocketAddr) -> Result<()> {
